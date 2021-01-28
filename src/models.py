779df2a4a55ea305b289efb5b4350ffd90af5a68
manu-neufeld <manuela.neufeld@gmail.com>
2021-01-28 10:48:19 +0000

    DB Instagram done

--- src/models.py
+++ src/models.py
@@ -8,38 +8,37 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class User(Base):
+follower = Table("follower", Base.Model.metadata,
+    Column("id_follower", Integer, ForeignKey("user.id"), primary_key=True),
+    Column("id_followed", Integer, ForeignKey("user.id"), primary_key=True)
+)
+
+class User(Base.Model):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    username = Column(String(15), nullable=False)
-    firstname = Column(String(50), nullable=False)
+    username = Column(String(80), unique=True, nullable=False)
+    firstname = Column(String(80), nullable=False)
     lastname = Column(String(100), nullable=True)
     email = Column(String(70), nullable=False)
 
-class Post(Base):
+class Post(Base.Model):
     __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey("user.id"))
 
-class Comment(Base):
+class Comment(Base.Model):
     __tablename__ = 'comment'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey("user.id"))
     post_id = Column(Integer, ForeignKey("post.id"))
     comment_text = Column(Text(), nullable=False)
 
-class Media(Base):
+class Media(Base.Model):
     __tablename__ = 'media'
     id = Column(Integer, primary_key=True)
     post_id = Column(Integer, ForeignKey("post.id"))
     media_type = Column(Enum("post","story","reel"))
     url = Column(String, nullable=False)
-
-class Follower(Base):
-    __tablename__ = 'follower'
-    id = Column(Integer, primary_key=True)
-    follower_user_id = Column(Integer, ForeignKey("user.id"))
-    followed_user_id = Column(Integer, ForeignKey("user.id"))
 
 
 ## Draw from SQLAlchemy base
